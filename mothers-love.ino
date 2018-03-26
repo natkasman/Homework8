641df4f73571217b5d771239fee5f89db0573327
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-03-26 12:43:06 -0700

    Create mothers-love.ino

---
+++ mothers-love.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+// the loop function runs over and over 
+void loop() {
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(6, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(5, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(5, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(6, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)   
+
+}
